9f937c03bb61916b0e87533656d2448dcec641c4
Pierre Wahlgren <pierre.wahlgren@gmail.com>
2014-08-27 18:09:29 +0200

    Minor refactoring

--- assets/js/app/modules/messages/index.js
+++ assets/js/app/modules/messages/index.js
@@ -21,8 +21,8 @@
 	 * the messages view of the app.
 	 */
 	ng.module('app.messages', [
-		'app.messages.message',		// Message item sub-module
-		'app.messages.copy'				// Message copy button sub-module
+		'app.messages.message',    // Message item sub-module
+		'app.messages.copy'        // Message copy button sub-module
 	])
 	.run(function () {
 		console.log( '`app.messages` init' );
